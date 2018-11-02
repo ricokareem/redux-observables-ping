0d9c69048e4b55e017a1992fdeffbeb3a4270d80
Rico Rodriquez Collins <rico@carbonfive.com>
2018-11-02 11:57:47 -0700

    add redux-observable ping exercise

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import { ping } from '../actions';
+
+let App = ({ isPinging, ping }) => (
+  <div>
+    <h1>is pinging: {isPinging.toString()}</h1>
+    <button onClick={ping}>Start PING</button>
+  </div>
+);
+
+App = connect(
+  ({ isPinging }) => ({ isPinging }),
+  { ping }
+)(App);
+
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+import 'rxjs';
+
+import App from './components/App';
+import Store from './store';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = Store();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
