52c70f8f46b2a3b77dd233be1c1296c791b2d601
Rico Rodriquez Collins <rico@carbonfive.com>
2018-11-02 14:06:19 -0700

    fix a-coupl-a things

--- src/components/App.js
+++ src/components/App.js
@@ -3,17 +3,14 @@ import { connect } from 'react-redux';
 
 import { ping } from '../actions';
 
-let App = ({ isPinging, ping }) => (
+const App = ({ isPinging, ping }) => (
   <div>
     <h1>is pinging: {isPinging.toString()}</h1>
     <button onClick={ping}>Start PING</button>
   </div>
 );
 
-App = connect(
+export default connect(
   ({ isPinging }) => ({ isPinging }),
   { ping }
 )(App);
-
-
-export default App;
